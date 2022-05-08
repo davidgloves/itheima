8db2922066957ab0ffd29274bd8ae85b39314629
DavidGuy <longfei_ge@163.com>
2022-05-08 21:50:21 +0800

    nothing

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,7 +5,16 @@ Page({
      * 页面的初始数据
      */
     data: {
+        userInfo:{}
+    },
 
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        const userInfo = wx.getStorageSync("userInfo");
+        this.setData({userInfo});
     },
 
     /**
@@ -19,13 +28,6 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
 
     },
 
